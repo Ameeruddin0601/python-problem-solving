7f35c065d42bd0b3a47fb38fe275ca6c74b339a4
Ameeruddin06 <ameerb0601@gmail.com>
2023-01-08 22:58:53 +0530

    update

--- factorial.py
+++ factorial.py
@@ -4,9 +4,17 @@
 #1.find factorial of given number
 #2.find no. of trailing zeros in that factorial
 
-def factorial(number):
-    fact = 1
-    for i in range(number, 0, -1):
-        fact = fact*i
-    return fact
-print(factorial(8)) 
+# def factorial(number):
+#     fact = 1
+#     for i in range(number, 0, -1):
+#         fact = fact*i
+#     return fact
+# print(factorial(8)) 
+
+#factorial using recursion
+def factorial(num):
+    if num==1:
+        return 1
+    else:
+        return(num*factorial(num-1))
+print(factorial(4))
